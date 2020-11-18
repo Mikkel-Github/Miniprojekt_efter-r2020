09b41cb67a38ae162a1ae52f7a9fcf6d29b90562
Lucas <Luhavenkvist@yahoo.dk>
2020-11-18 11:44:39 +0100

    ændre akuatorer

--- miniprojekt_2020/src/com/company/Main.java
+++ miniprojekt_2020/src/com/company/Main.java
@@ -9,9 +9,19 @@ public class Main {
         sensor.addSensor("test1");
         sensor.addSensor("test2");
 
+        Aktuator aktuator = new Aktuator();
+
+        aktuator.addAktuator("aku1");
+        aktuator.addAktuator("aku2");
+        aktuator.addAktuator("aku3");
+
         sensor.updateSensor(1, "tis");
+        aktuator.updateAktuator(1, "pis");
         for (String s:sensor.getSensor()) {
             System.out.println(s);
+        }
+        for (String a:aktuator.getAktuator()) {
+            System.out.println(a);
         }
     }
 }
